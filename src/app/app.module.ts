33706b75d1d4eb90a02141e425f80f2c30aa660b
Raul Reyes <raul.remo02@gmail.com>
2019-11-07 11:28:19 -0600

    Modificacion rutas, y listo para tag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+// Modulos
+import { PagesModule } from './pages/pages.module';
+// Rutas
+import { APP_ROUTES } from './app-routing.module';
+// Componentes
+import { RegisterComponent } from './login/register.component';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { GraficasComponent } from './pages/graficas/graficas.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PagenotfoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    GraficasComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
